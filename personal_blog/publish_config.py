1a2502c510abf97663e809d1291dffc129774b66
Vladimir Shteyn <MisterVladimir@users.noreply.github.com>
2022-12-08 22:51:11 -0500

    Pelican initial setup (#2)

---
+++ personal_blog/publish_config.py
@@ -0,0 +1,18 @@
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+from personal_blog.pelican_config import *
+
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = "https://vladimir.shteyn.net"
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
